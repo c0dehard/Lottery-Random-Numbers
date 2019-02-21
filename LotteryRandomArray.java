ab5e69f28bf3239242bb5a355c1fa19d227da168
c0dehard <47797039+c0dehard@users.noreply.github.com>
2019-02-21 21:21:58 +0100

    Improvement

--- LotteryRandomArray.java
+++ LotteryRandomArray.java
@@ -1,23 +1,25 @@
 import java.util.Random;
+import static java.lang.System.out;
 
 /**
- *
  * @author c0dehard
  */
+
 public class LotteryRandomArray {
 
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
 
         Random r = new Random();
 
-        byte[] Zahlen = new byte[6];
+        byte[] Numbers = new byte[6];
 
-        for (byte Anzahl : Zahlen) {
-            System.out.print(Zahlen[Anzahl] = (byte) r.nextInt(49));
-              System.out.print(" "); //Separiert die Zahlen
+        for (byte Number : Numbers) {
+            out.print(Numbers[Number] = (byte) r.nextInt(49));
+              out.print(" "); //Seperates the numbers
             
         }
 
